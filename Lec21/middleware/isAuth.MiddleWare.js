2609db2d670673a6878a58a2cf841a8821c57297
lizigigauri01 <lizi.gigaurite01@geolab.edu.ge>
2026-07-17 03:57:15 +0400

    .

--- Lec21/middleware/isAuth.MiddleWare.js
+++ Lec21/middleware/isAuth.MiddleWare.js
@@ -1,10 +1,12 @@
 const jwt = require("jsonwebtoken")
 
+
 function getToken(headers) {
     if (!headers["authorization"]) return null
     const [type, token] = headers["authorization"].split(" ")
     return type === "Bearer" ? token : null
 }
+
 
 async function isAuth(req,res,next) {
     const token = getToken(req.headers)
