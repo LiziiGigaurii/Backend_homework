ac479cc77c3828aa5238929b80abe926b65e3e15
Lizi Gigauri <126089107+LiziiGigaurii@users.noreply.github.com>
2026-08-27 21:40:04 +0400

    start

--- lec31/dist/app.module.js
+++ lec31/dist/app.module.js
@@ -5,34 +5,21 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.AppModule = exports.ObserveInstrument = exports.ObserveModule = void 0;
+exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
-const config_1 = require("@nestjs/config");
-const mongoose_1 = require("@nestjs/mongoose");
-const observe_1 = require("@nestjs/observe");
 const app_controller_1 = require("./app.controller");
 const app_service_1 = require("./app.service");
 const users_module_1 = require("./users/users.module");
 const products_module_1 = require("./products/products.module");
-_a = (0, observe_1.createObserveModule)(), exports.ObserveModule = _a.ObserveModule, exports.ObserveInstrument = _a.ObserveInstrument;
+const config_1 = require("@nestjs/config");
+const mongoose_1 = require("@nestjs/mongoose");
 let AppModule = class AppModule {
 };
 exports.AppModule = AppModule;
 exports.AppModule = AppModule = __decorate([
     (0, common_1.Module)({
-        imports: [
-            exports.ObserveModule.forRoot({
-                appKey: 'YOUR_APP_KEY',
-                appSecret: 'YOUR_APP_SECRET',
-                serviceId: 'lec31',
-            }),
-            config_1.ConfigModule.forRoot({ isGlobal: true }),
-            mongoose_1.MongooseModule.forRoot(process.env.MONGO_URL),
-            users_module_1.UsersModule,
-            products_module_1.ProductsModule,
-        ],
+        imports: [config_1.ConfigModule.forRoot({ isGlobal: true }), mongoose_1.MongooseModule.forRoot(process.env.MONGO_URI), users_module_1.UsersModule, products_module_1.ProductsModule],
         controllers: [app_controller_1.AppController],
         providers: [app_service_1.AppService],
     })
--- lec31/src/app.module.ts
+++ lec31/src/app.module.ts
@@ -1,28 +1,13 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
-import { createObserveModule } from '@nestjs/observe';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
-
-export const { ObserveModule, ObserveInstrument } = createObserveModule();
+import {ConfigModule} from "@nestjs/config"
+import {MongooseModule} from "@nestjs/mongoose"
 
 @Module({
-  imports: [
-    // Distributed tracing, auto-correlated logs, request/job metrics, error
-    // telemetry, alarms, and more — out of the box. Sign up at https://observe.nestjs.com
-    ObserveModule.forRoot({
-      appKey: 'YOUR_APP_KEY',
-      appSecret: 'YOUR_APP_SECRET',
-      serviceId: 'lec31',
-    }),
-    ConfigModule.forRoot({ isGlobal: true }),
-    MongooseModule.forRoot(process.env.MONGO_URL!),
-    UsersModule,
-    ProductsModule,
-  ],
+  imports: [ConfigModule.forRoot({isGlobal:true}),MongooseModule.forRoot(process.env.MONGO_URI!),UsersModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
